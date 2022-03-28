788d435f56aaba4ff0a4bd89da15a0b8306eed5b
HoangVan <87821034+HoangVan2002@users.noreply.github.com>
2022-03-28 08:14:21 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,6 +1,6 @@
 /* # Snake
 Ngôn ngữ C/C++
-Tui viết có thể chưa được tốt, chưa được tối ưu, bạn có thêm chỉnh sửa chút xíu cho nó xịn xò con bò hơn nhí :3
+Tui có tham khảo vài hàm trên mạng và viết có thể chưa được tốt, chưa được tối ưu, bạn có thêm chỉnh sửa chút xíu cho nó xịn xò con bò hơn nhí :3
 */
 #pragma warning(disable : 4996)
 #include <cstdio>
